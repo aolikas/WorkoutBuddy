cdab4a49fbcb935e2e7f872141fd7f4064c7484a
aolikas <olkaspark@gmail.com>
2021-10-14 09:30:34 +0200

    added layouts

---
+++ app/src/main/java/my/aolika/workoutbuddy/auth/LoginFragment.kt
@@ -0,0 +1,13 @@
+package my.aolika.workoutbuddy.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import my.aolika.workoutbuddy.R
+
+
+class LoginFragment : Fragment() {
+
+}
---
+++ app/src/main/java/my/aolika/workoutbuddy/auth/RegistrationFragment.kt
@@ -0,0 +1,12 @@
+package my.aolika.workoutbuddy.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import my.aolika.workoutbuddy.R
+
+
+class RegistrationFragment : Fragment() {
+}
